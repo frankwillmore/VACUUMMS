792272143bb9d34372792dc39e5dd566e1541d1b
Frank Willmore <frankwillmore@gmail.com>
2025-08-11 13:47:58 -0500

    setting up examples for Jupyter and python

---
+++ examples/jupyter/README.md
@@ -0,0 +1,8 @@
+# Running VACUUMMS in a jupyter notebook
+
+Make sure that you have created a kernel based on a python environment, and that it includes matplotlib for inline plots. Plots can then be enabled by adding the following to a cell in Jupyter:
+
+    %matplotlib inline
+    import matplotlib as plt
+
+
---
+++ examples/python/polystyrene.py
@@ -0,0 +1,29 @@
+import vacuumms as v
+f=v.LAMMPSConfiguration("equilibrated_system.data")
+type(f)
+f.getSize()
+f.getBoxDimensions()
+f.cram()
+f.isCrammed()
+f
+p = v.Parameters(["-n", "100", "-box", "4.24264", "4.24264", "4.24264"])
+p
+p.addParameter("-box  87.578857  87.578857 87.578857")
+p
+p.toFile("foo.prm")
+p=v.Parameters("foo.prm")
+p
+o = v.DDX(c, p)
+
+o = v.DDX(f, p)
+o.execute()
+result=o.getResult()
+
+csdp=v.Parameters(['-width', '0.1', '-n_bins', '50'])
+csd=v.CavitySizeDistribution(result, csdp)
+
+csd
+o
+p
+csd.writeToFile('foo.bar')
+
